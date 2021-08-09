d034db7eed7c6a76a3a81d37275690c689d09d17
VickyZhou2021 <88239315+VickyZhou2021@users.noreply.github.com>
2021-08-10 03:13:20 +0800

    Add files via upload

--- app/src/main/java/com/example/asfzu/MainActivity.java
+++ app/src/main/java/com/example/asfzu/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button1=(Button)findViewById(R.id.Bu_3);
+        Button button2=(Button)findViewById(R.id.Bu_1);
+        Button button3=(Button)findViewById(R.id.Bu_2);
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent=new Intent(MainActivity.this,MainActivity2.class);
                 startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"You will log in later..",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"You will log in later...",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
